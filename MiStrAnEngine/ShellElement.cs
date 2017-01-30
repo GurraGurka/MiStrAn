d2c2749b90363f0d164d21dfefe19ce5d2b367e9
GurraGurka <gustav.g@gmail.com>
2017-01-30 14:49:13 +0100

    Small Changes

---
+++ MiStrAnEngine/ShellElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiStrAnEngine
+{
+    public class ShellElement
+    {
+        public List<Node> nodes;
+        public int Id;
+
+        public Matrix D;
+
+        public ShellElement(List<Node> _nodes, int _id)
+        {
+            nodes = _nodes;
+            Id = _id;
+        }
+
+    }
+}
---
+++ MiStrAnGH/StaticFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace MiStrAnGH
+{
+    public static class StaticFunctions
+    {
+        public static MiStrAnEngine.Structure ConvertGHMeshToStructure(Mesh m)
+        {
+
+            return new MiStrAnEngine.Structure();
+        }
+
+    }
+}
