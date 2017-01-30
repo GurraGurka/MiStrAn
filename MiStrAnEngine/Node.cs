18ab3434738e380a3e6b391c6520f7385438aa6c
GurraGurka <gustav.g@gmail.com>
2017-01-30 14:18:48 +0100

    Updates on Node class
    Added functionality

--- MiStrAnEngine/Node.cs
+++ MiStrAnEngine/Node.cs
@@ -8,6 +8,61 @@ namespace MiStrAnEngine
 {
     public class Node
     {
-        public double i;
+
+        // Field variables
+        public double x;
+        public double y;
+        public double z;
+
+        public int dofX;
+        public int dofY;
+        public int dofZ;
+        public int dofXX;
+        public int dofYY;
+        public int dofZZ;
+
+
+        private int id;
+
+        // Gets and sets
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+
+            set
+            {
+                id = value;
+                dofX = id * 6 - 5;
+                dofY = id * 6 - 4;
+                dofZ = id * 6 - 3;
+                dofXX = id * 6 - 2;
+                dofYY = id * 6 - 1;
+                dofZZ = id * 6;
+            }
+        }
+
+        // Constructors
+        public Node(double _x, double _y, double _z, int _id)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+
+            id = _id;
+        }
+
+
+        // Sets id to -1
+        public Node(double _x, double _y, double _z) : this(_x, _y, _z, -1)
+        { }
+
+
+
+
     }
 }
