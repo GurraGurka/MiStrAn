f8c66b0db60adcf30747d096d5da062c5f1b3ac4
JimmieAndersson <jimmiealexanderandersson@gmail.com>
2017-02-01 16:55:37 +0100

    Lite Global K matris, inget vettigt

--- MiStrAnEngine/StiffnessMatrix.cs
+++ MiStrAnEngine/StiffnessMatrix.cs
@@ -13,20 +13,14 @@ namespace MiStrAnEngine
         //BASED ON 4 POINT ELEMENTS
         //PLANE STRESS CHOSEN, WE CAN CHANGE TO PLANE STRAIN ALSO
         //NO BODY FORCE IN X- AND Y-DIRECTION (SAME AS ASSIGNMENT 3)
-        // D MATRIX BASED ON HOOKE PLANE STRESS
+        // D MATRIX BASED ON HOOKE PLANE STRESS (OTHERWISE CHANGE)
 
-        //THIS SHOULD BE AN INPUT FROM GRASSHOPPER
-        double E = 210 * Math.Pow(10, 9);
-        double v = 0.3; //Maybe make this input
-
-        public static Matrix GetMeloshMatrix(Structure struc)
+        
+        //BASED ON PLANRE
+        public static Matrix planre(ShellElement shell, double thickness,Matrix D)
         {
             //Add so we can get the list with shell elements from the structure
-            List<ShellElement> shellList = struc.shells;
-
-
-            foreach (ShellElement shell in shellList)
-            {
+            
                 List<Node> shellNodes = shell.nodes;
 
                 //GÖR EN KONTROLL SÅ ATT DET VERKLIGEN BARA ÄR 4 (3 I FRAMTIDEN) NODER I DENNA LISTA
@@ -41,9 +35,24 @@ namespace MiStrAnEngine
                 Matrix ygp = new Matrix(new double[,] { { -1, -1, 1, 1 } });
                 ygp = (1 / Math.Sqrt(3)) * ygp;
 
-                //Constitutive matrix
-                Matrix D = E/(1-Math.Pow(v,2))*
-            }
+              
+
+                Matrix Ke = Matrix.ZeroMatrix(8, 8); //8x8 matrix
+                
+                
+                for(int i=0;i<4;i++)
+                {
+                    double x = a * xgp[0, i];
+                    double y = b * ygp[0, i];
+                    Matrix B = new Matrix(new double[,] { { -(b-y), 0, b-y,0,b+y,0,-(b+y),0 }, { 0, -(a-x), 0, -(a+x), 0, a+x,0,a-x }, { -(a-x), -(b-y), -(a+x),b-y,a+x,b+y,a-x, (-b+y) } });
+                    B = 1/(4*a*b)*B;
+
+                    Ke = Ke + a * b * thickness*Matrix.Transpose(B) * D * B;
+                }
+
+            // Matrix fe = new Matrix(new double[,] { {bx}, {by}, {bx}, { by }, { bx }, { by }, { bx }, { by } });
+            Matrix fe = new Matrix(new double[,] { { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 } });
+            fe = a * b * thickness * fe;
 
             return new Matrix(1,1);
         }
